5d49d01f9bcd2cc8795143c204ccff588eceb2c2
CarlosJHR64 <carlosjhr64@gmail.com>
2011-06-21 07:24:35 -0700

    restructure and debug

--- lib/shredder.rb
+++ lib/shredder.rb
@@ -30,66 +30,6 @@ module Shredder
     return count
   end
 
-  class Streams
-    # this one takes streams
-    def initialize(sew,shreds,limit=0)
-      @sew = sew
-      @shreds = shreds
-      @limit = limit
-    end
-
-    def shred(limit=@limit)
-      Shredder.shred(@sew,@shreds,limit)
-    end
-
-    def sew(limit=@limit)
-      Shredder.sew(@sew,@shreds,limit)
-    end
-  end
-
-  class Files
-     # this one takes filenames
-    def initialize(sew,shreds,limit=0)
-      @sew = sew
-      @shreds = shreds
-      @limit = limit
-    end
-
-    def shred(limit=@limit)
-      reader = File.open(@sew,'r')
-      writers = []
-      @shreds.each{|shred| writers.push( File.open(shred,'wb') ) }
-
-      count = nil
-      begin
-        count = Shredder.shred( reader, writers, limit )
-      rescue Exception
-        raise $!
-      ensure
-        writers.each{|writer| writer.close}
-        reader.close
-       end
-
-      return count
-    end
-
-    def sew(limit=@limit)
-      writer = File.open(@sew,'wb')
-      readers = []
-      @shreds.each{|shred| readers.push( File.open(shred,'r') ) }
-
-      count = nil
-      begin
-        count = Shredder.sew( writer, readers, limit )
-      rescue Exception
-        raise $!
-      ensure
-        writer.close
-        readers.each{|reader| reader.close}
-      end
-
-      return count
-    end
-  end
-
+  autoload :Files,	'shredder/files'
+  autoload :Streams,	'shredder/streams'
 end
