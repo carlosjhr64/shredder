f562c7bff381f797048ddd74b13352cb1cf53eac
CarlosJHR64 <carlosjhr64@gmail.com>
2021-09-28 13:27:29 -0700

    Allow initial xor to be set.

--- lib/shredder/shredder.rb
+++ lib/shredder/shredder.rb
@@ -3,11 +3,11 @@ module Shredder
 
   # note that these are streams
   def shred(sewn=(@sewn or $stdin), shreds=@shreds,
-            writers: shreds, reader: sewn, limit: 0)
-    shreds,xor,count = writers.length,0,0
+            writers: shreds, reader: sewn, limit: 0, xor: 0)
+    shreds,count = writers.length,0
     while byte = reader.getbyte do
       writers[count%shreds].putc(xor^(xor=byte))
-      count += 1 # not that 0 is skipped
+      count += 1 # note that 0 is skipped
       break if count == limit
     end
     return count
@@ -15,8 +15,8 @@ module Shredder
 
   # note that these are streams
   def sew(shreds=@shreds, sewn=(@sewn or $stdout),
-          readers: shreds, writer: sewn, limit: 0)
-    shreds,xor,count = readers.length,0,0
+          readers: shreds, writer: sewn, limit: 0, xor: 0)
+    shreds,count = readers.length,0
     while byte = readers[count%shreds].getbyte do
       writer.putc(xor=(byte^xor))
       count += 1 # note that 0 is skipped
